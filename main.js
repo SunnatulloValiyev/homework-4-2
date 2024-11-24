813bafa445e1f5513e5066cd80921e1f7eb1ab4f
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2024-11-24 20:48:48 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,415 @@
+// 1-masala
+
+// let num = +prompt("Son kiriting");
+
+// if (num > 0) {
+//     num++;
+//     console.log(num);
+// }
+
+
+// 2-masala
+
+// let num = +prompt("Son kirting");
+
+// if (num < 0) {
+//     num--;
+//     console.log(num);
+// }else{
+//     console.log(0);
+// }
+
+// 3-masal
+
+// let num = +prompt("Son kiriting");
+
+// if (num == 0){
+//     console.log(1);
+// }
+// else {
+//     console.log(0)
+// }
+
+
+// 4-masala
+
+// let num = +prompt("Son kiriting");
+
+// if (num > 100){
+//     console.log(1); 
+// }   
+// else {
+//     console.log(0);
+// }
+
+
+// 5-masala
+// let num = +prompt("son kiriting:")
+
+// if (num < 10) {
+//     console.log(-1);
+// }
+// else {
+//     console.log(num);
+// }
+
+
+// 6-misol
+
+// let num = +prompt("Son kiritng:")
+
+// if (num > 0){
+//     console.log(num*num);
+// }
+// else {
+//     console.log(0);
+// }
+
+
+// 7-misol
+
+// let num = +prompt("Son kirting:")
+
+// if (num < 0){
+//     console.log(Math.abs(num));
+// }
+// else {
+//     console.log(num)
+// }
+
+// 8-misol
+
+// let num = +prompt("Son kirting:")
+
+// if (num == 0){
+//     console.log(10);    
+// }
+// else {
+//     console.log(num);
+// }
+
+// 9-misol
+
+// let num = +prompt("Son kiriting")
+
+// if (num > 1) {
+//     console.log(num ** 3);
+// }    
+
+// 10-misol
+
+// let num = +prompt("Son kirting")
+
+// if (num < 0) {
+//     console.log(-10);
+// }
+// else {
+//     console.log(10);
+// }
+
+
+// 11-misol
+
+// let num = +prompt("Son kirting:")
+
+// if (num % 2 == 0) {
+//     console.log(2);
+// }
+// else {
+//     console.log(1);
+// }
+
+// 12-misol
+
+// let num = +prompt("Son kiriting");
+
+// if (num % 2 == 0){
+//     console.log(4);
+// }
+// else {
+//     console.log(3);
+// }
+
+// 13-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num % 2 == 0 && num > 10){
+//     console.log(1)
+// }
+// else {
+//     console.log(num)
+// }
+
+// 14-misol
+
+// let num = +prompt("Son kiritng:")
+
+// if (num % 2 == 0 && num < 0) {
+//     console.log(num);
+// }
+// else{
+//     console.log(-1);    
+// }
+
+
+// 15-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num % 2 == 0) {
+//     if(num % 5==0){
+//         console.log(100);
+//     }
+// }
+// else {
+//     console.log(num);
+// }
+
+
+// 16-misol
+
+// let num = +prompt("Son kiriting")
+
+// if (num % 2 != 0) {
+//     console.log(num ** 2);
+// }
+
+
+
+// 17-misol
+
+// let num = +prompt("Son kiriting")
+
+// if (num % 2 == 0) {
+//     console.log(num ** 3);
+// }
+
+// 18-misol
+
+// let num = +prompt("Son kirting:")
+
+// if (num % 2 == 0 && num > 20){
+//     console.log(50);
+// }
+// else {
+//     console.log(-50);
+// }
+
+// 19-misol
+
+// let num = +prompt("Son kirting:")
+
+// if (num % 2 != 0) {
+//     if (num % 7 === 0){
+//         console.log(7)
+//     }
+// }
+
+// 20-misol
+
+// let num = +prompt("Son kirting:")
+
+// if (num % 2 == 0 && num < 0){
+//         console.log(-20);
+//     }
+
+
+// 21-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 == num2){
+//     console.log(1);
+// }
+// else {
+//     console.log(0);
+// }
+
+
+// 22-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+
+// if (num1 > num2) {
+//     console.log(2);
+// }
+// else {
+//     console.log(3);
+// }
+
+// 23-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 < num2 && num1 > 0){
+//     console.log(10);
+// }
+
+// 24-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 > num2 && num1 && num2 % 2 == 0){
+//     console.log(5);
+// }
+
+
+// 25-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 && num2 > 0){
+//     console.log(num1 + num2);
+// }
+
+
+// 26-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 < num2 && num2 % 2 == 0){
+//     console.log(-1)
+// }
+
+
+// 27-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 == num2){
+//     console.log(100);
+// }
+// else {
+//     console.log(-100);
+// }
+
+// 28-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 % 10 === 0 && num2 % 5 ===0){
+//     console.log(50);
+// }
+
+// 29-misol
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 == num2*2) {
+//     console.log(2);
+// }   
+
+
+// 30-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// if (num1 < num2 && num2 % 2 == 1){
+//     console.log(-10);
+// }
+
+// 31-misol
+
+// let num = +prompt("Son kirting")
+
+// if(num > 0 && num % 3 == 0 ) {
+//     console.log(3);
+// }
+// else{
+//     console.log(0);
+// }
+
+// 32-misol
+
+// let num = +prompt("Son kirting")
+
+// if (num < 0 && num % 2 == 0){
+//     console.log(-2);
+// }
+
+
+// 33-misol
+
+// let num = +prompt("Son kiriting")
+
+// if (num > 0 && num % 5 === 0){
+// }
+// console.log(num%5);
+    
+
+// 34-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num > 0 && num < 10){
+//     console.log(num * 2);
+// }
+
+
+// 35-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num % 2 == 0 || num % 4 == 0) {
+//     console.log(1)
+// }
+
+
+// 36-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num % 2 == 1 && num > 15){
+//     console.log(15) 
+// }
+
+
+// 37-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num % 2 == 0 && num % 3 == 0){
+//     console.log(6);
+// }
+// else {
+//     console.log(1);
+// }
+
+// 38-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num > 0 && num % 7 == 0){
+//     console.log(14)
+// }
+
+// 39-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (num > 0 || num === 0){
+//     console.log(0);
+// }
+// else {
+//     console.log(-5);
+// }
+
+// 40-misol
+
+let num = +prompt("Son kiriting:")
+
+if (num < 0 && num % 10 != 0){
+    console.log(-3);
+    
+}
